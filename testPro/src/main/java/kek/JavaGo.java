3330c45dd017ed0566a86609cb2706c34a326839
Ecl1pce <vvaskez@yandex.ru>
2016-09-15 19:23:28 +0300

    работай

---
+++ testPro/src/main/java/kek/JavaGo.java
@@ -0,0 +1,14 @@
+package kek;
+
+/**
+ * Created by Владелец on 14.09.2016.
+ */
+public class JavaGo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    int crazyFunction(int a) {
+        return a + 1;
+    }
+}
